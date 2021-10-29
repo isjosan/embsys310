10e9cae763e4190c43f35139a0339b91265240cc
Iqbal Singh Josan <Iqbal.singh@uspurtek.com>
2021-10-29 21:09:34 +0000

    upload blinking-led main.c

---
+++ assignment03/blinking-led/main.c
@@ -0,0 +1,37 @@
+/* This program blinks LED1 in an infinite loop */
+#define RCC_BASE 0x40021000 /*RCC (Reset and Clock Control) Register Base Address */
+#define RCC_AHB2ENR (RCC_BASE + 0x4C) /* Peripheral Clock Enable Register Address*/
+#define GPIOA_BASE 0x48000000 /* GPIOA Base Address */
+#define GPIOA_MODER GPIOA_BASE /* GPIOA Port Mode Register Address */
+#define GPIOA_ODR (GPIOA_BASE + 0x14) /* GPIOA Port Output Data Register */
+#define RCC_AHB2ENR_DATA 0x00000001 /* Reset Value: 0x00000000 */
+#define GPIOA_MODER_DATA 0x00000400 /* Reset Value: 0xABFFFFFF */
+#define GPIOA_ODR_LED_ON_DATA 0x00000020 /* Reset Value: 0x00000000 */
+#define GPIOA_ODR_LED_OFF_DATA 0x00000000
+
+const unsigned int DLY = 1000000;
+
+void delay(unsigned int max_count);
+
+void main()
+{
+  *((unsigned int*)RCC_AHB2ENR) |= RCC_AHB2ENR_DATA; /* Enable Clock Gating for GPIOA */
+  *((unsigned int*)GPIOA_MODER) &= GPIOA_MODER_DATA; /* Set PA5 for LED1 for OUTPUT */
+  
+  while(1)
+  {
+      *((unsigned int*)GPIOA_ODR) = GPIOA_ODR_LED_ON_DATA; /* Turn ON LED1 */
+      delay(DLY);
+      
+      *((unsigned int*)GPIOA_ODR) = GPIOA_ODR_LED_OFF_DATA; /* Turn OFF LED1 */
+      delay(DLY);
+  }  
+}
+
+void delay(unsigned int max_count)
+{
+    unsigned int count = 0;
+    
+    for(count = 0; count < max_count ; ++count);
+}
+        
