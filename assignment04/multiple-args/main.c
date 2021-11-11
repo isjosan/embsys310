e5e030610f2f154bf74b991822d80058382d03c4
Iqbal Singh Josan <Iqbal.singh@uspurtek.com>
2021-11-11 03:50:18 +0000

    Add files via upload

---
+++ assignment04/multiple-args/main.c
@@ -0,0 +1,32 @@
+#define SIZE 5
+int powers[SIZE];
+
+void func1(int num1, int num2, int num3, int num4, int num5);
+void func2(void);
+
+int main()
+{
+    func2();
+    
+    return 0;
+}
+
+void func2(void)
+{
+    int num1 = 2;
+    int num2 = 4;
+    int num3 = 8;
+    int num4 = 16;
+    int num5 = 32;
+    
+    func1(num1, num2, num3, num4, num5);
+}
+
+void func1( int num1, int num2, int num3, int num4, int num5)
+{
+    powers[0] = num1;
+    powers[1] = num2;
+    powers[2] = num3;
+    powers[3] = num4;
+    powers[4] = num5;
+}
