f0c29912647498a611e9e7ec8353bd66e062e969
Iqbal Singh Josan <Iqbal.singh@uspurtek.com>
2021-11-11 18:13:22 +0000

    Update main.c

--- assignment03/blinking-led/main.c
+++ assignment03/blinking-led/main.c
@@ -13,7 +13,7 @@ const unsigned int DLY = 1000000;
 
 void delay(unsigned int max_count);
 
-void main()
+int main()
 {
   *((unsigned int*)RCC_AHB2ENR) |= RCC_AHB2ENR_DATA; /* Enable Clock Gating for GPIOA */
   *((unsigned int*)GPIOA_MODER) &= GPIOA_MODER_DATA; /* Set PA5 for LED1 for OUTPUT */
