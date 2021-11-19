2bffb10656e0c6c12b5acab29782140d4696a602
Iqbal Singh Josan <Iqbal.singh@uspurtek.com>
2021-11-19 00:07:29 +0000

    Add files via upload

---
+++ assignment05/swap-pointers-C/main.c
@@ -0,0 +1,32 @@
+// The swap_pointer() function swaps the poniters for int variables x & y.
+void swap_pointer(int** arg1, int** arg2);
+
+int main(void)
+{  
+    int x = 2000000;
+    int y = 1000000;
+    
+    int* xPtr;
+    xPtr = &x;
+    int* yPtr;
+    yPtr = &y;
+
+    int** PtrxPtr;
+    int** PtryPtr;
+    PtrxPtr = &xPtr;
+    PtryPtr = &yPtr;
+    
+    swap_pointer(PtrxPtr, PtryPtr);
+    
+    return 0;
+}
+
+void swap_pointer(int** arg1, int** arg2)
+{   
+    int* temp;
+    
+    temp = *arg2;
+    *arg2 = *arg1;
+    *arg1 = temp;
+}
+    
